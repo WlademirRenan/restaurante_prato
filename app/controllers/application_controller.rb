27fadd70b0174f124c3b946cdc8425ee1fd3b4fc
Wlademir Renan dos Santos <wlademir.renan@gmail.com>
2016-09-03 18:33:27 -0300

    configs iniciais

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  #before_action :validar_login
+  before_action :authenticate_user!
+
+  def validar_login
+    if current_user.present?
+    #  (acesso normal, deixo o padrao do codigo)
+    else
+      redirect_to '/users/sign_in'
+    end
+
+  end
 end
